3d21456d7b0a9110999fa0797853ffa48db4da1d
Vectoras <johnny.boghean.developer@gmail.com>
2024-06-16 19:46:33 +0100

    basic app structure

---
+++ src/app/app-page/app-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-page',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app-page.component.html',
+  styleUrl: './app-page.component.scss'
+})
+export class AppPageComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// importing components
+import { AppNavComponent } from './app-nav/app-nav.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppPageComponent } from './app-page/app-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AppNavComponent, AppFooterComponent, AppPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Assessment';
 }
